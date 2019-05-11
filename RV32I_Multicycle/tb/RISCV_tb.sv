b3d393ff2b69c79ae7683543a8ffc09a12d0b171
Chris Griffiths <cgriff32@gmail.com>
2019-05-11 16:50:29 +0100

    Housekeeping

--- RV32I_Multicycle/tb/RISCV_tb.sv
+++ RV32I_Multicycle/tb/RISCV_tb.sv
@@ -5,16 +5,10 @@ module RISCV_tb;
 
 	logic clk;
 	logic [31:0] 	debug1;
-logic [31:0] 	debug2;
-logic [31:0] 	debug3;
-logic [31:0] 	debug4;
 
-RISCV_Multicore DUT(
+RISCV_Multicycle DUT(
   .clk(clk),
-  .debug1(debug1),
-  .debug2(debug2),
-  .debug3(debug3),
-  .debug4(debug4)
+  .debug1(debug1)
   );
 
 initial
